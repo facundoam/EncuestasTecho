0c3d51f4608b9e36bb3372b307f7fd350e78eb6f
Facundo Arias <facundoarias@Facundos-Mac.local>
2017-12-25 23:04:08 -0800

    asd

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { PedidosEncuestaServiceProvider } from '../../providers/pedidos-encuesta-service/pedidos-encuesta-service';
 import { Geolocation } from '@ionic-native/geolocation';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   listaPedidos = [];
 
-  constructor(private pedidosService: PedidosEncuestaServiceProvider, private geolocation: Geolocation) {
+  constructor(private alertCtrl: AlertController, private pedidosService: PedidosEncuestaServiceProvider, private geolocation: Geolocation) {
     this.ionViewDidLoad();
   }
 
@@ -21,12 +23,23 @@ export class HomePage {
   getCurrentPosition(){
     let posOptions = {timeout: 10000, enableHighAccuracy: false};
     this.geolocation.getCurrentPosition(posOptions).then((resp) => {
-      console.log(resp.coords.latitude);
-      console.log(resp.coords.longitude);
+      let latitude = resp.coords.latitude;
+      let longitude = resp.coords.longitude;
+      this.showAlert(latitude,longitude);
     }).catch((error) => {
       console.log('Error getting location', error);
     });
+
   }
+
+  showAlert(lat,lng) {
+   let alert = this.alertCtrl.create({
+     title: 'New Friend!',
+     subTitle: 'lat: ' + lat + '  lng:' + lng ,
+     buttons: ['OK']
+   });
+   alert.present();
+ }
 
   getPedidosEncuestas(){
     this.pedidosService.getPedidosEncuestas().subscribe(data => this.listaPedidos = data);
