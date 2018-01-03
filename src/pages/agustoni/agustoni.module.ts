cc23da68284e0f8e39891bc1a46aad51e0086317
Facundo Arias <facundoarias@Facundos-Mac.local>
2018-01-03 15:18:37 -0800

    asdas2

---
+++ src/pages/agustoni/agustoni.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AgustoniPage } from './agustoni';
+
+@NgModule({
+  declarations: [
+    AgustoniPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AgustoniPage),
+  ],
+})
+export class AgustoniPageModule {}
